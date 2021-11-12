2f86c715a586ca0c84cc1cf122683742913fb964
unknown <raveen.anand@whitehatjr.com>
2021-11-12 17:37:51 +0530

    V2

--- Tic-Tac-Toe/sketch.js
+++ Tic-Tac-Toe/sketch.js
@@ -1,25 +1,18 @@
-// An array of all the colors used in this code.
 var turn=0
 function setup() {
-    //size of the canvas to draw on
     createCanvas(innerWidth,innerHeight); 
-    // Set the background to white, and move the line to setup();
     background("yellow");
-
     fill("#404040");
-  rect(0, 0, width, 50);
-
-  textAlign(CENTER);
-  fill("White");
-  textSize(18);
-  text("TIC TAC TOE", width / 2, 33);
-  fill("Black");
+    rect(0, 0, width, 50);
+    textAlign(CENTER);
+    fill("White");
+    textSize(18);
+    text("TIC TAC TOE", width / 2, 33);
+    fill("Black");
 }
-
 
 function draw()
 {
-
  createRow(150)
  createRow(250)
  createRow(350)  
@@ -33,8 +26,3 @@ function createRow(y)
        b1.appear();   
    }  
 }
-
-
-
-    
-
